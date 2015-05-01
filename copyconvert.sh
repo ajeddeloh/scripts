accb8123f2e97d30c7263ac0c88e4a93c1d548f1
Andrew Jeddeloh <andrewplayssax@gmail.com>
2015-04-30 21:13:06 -0400

    music sorter is now parallel

--- copyconvert.sh
+++ copyconvert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+COMMANDS='/tmp/commands'
+
 mkdir -p $2
 pushd "$1"
 
@@ -9,15 +11,16 @@ find . -print0 | while IFS= read -r -d $'\0' line; do
     fi
     if [[ -f "$line" ]]; then
         TYPE="$(mimedb -f $line | grep -i flac)"
-        echo ">$TYPE<"
         if [[ -n "$TYPE" ]]; then
             OUT=$2/$(echo $line | sed s/.flac/.ogg/I)
-            ffmpeg -loglevel quiet -y -i "$line" -acodec libvorbis "$OUT" &
+            echo "ffmpeg -loglevel quiet -y -i \"$line\" -acodec libvorbis \"$OUT\"" >> $COMMANDS &
         else
-            true
-            ln -s "$line" "$2/$line"
+            ln -s "$1/$line" "$2/$line"
         fi
     fi
 done
 
+PARALLEL=-j9 parallel --bar -a $COMMANDS
+
+rm $COMMANDS
 popd 
