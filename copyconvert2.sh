2bbf5e95a6e58ca523898b66aba122d14c597a83
Andrew Jeddeloh <andrewplayssax@gmail.com>
2015-11-03 19:24:18 -0800

    added linking step to copyconvert

--- copyconvert2.sh
+++ copyconvert2.sh
@@ -3,4 +3,7 @@ find . -type d -exec mkdir -p /media/MusicLossy/{} \;
 popd > /dev/null
 #oh god why
 find /media/Music -type f -name "*.flac" -print0 | xargs -P8 -I '{}' -0 \
-    bash -c 'echo "$1" && ffmpeg -hide_banner -loglevel quiet -y -i "$1"  -acodec libvorbis "$(echo "$1" | sed s/^\\\/media\\\/Music/\\\/media\\\/MusicLossy/ | sed s/\.flac/\.ogg/)"' - '{}'
+    bash -c 'echo "$1" && ffmpeg -hide_banner -loglevel quiet -n -i "$1"  -acodec libvorbis "$(echo "$1" | sed s/^\\\/media\\\/Music/\\\/media\\\/MusicLossy/ | sed s/\.flac/\.ogg/)"' - '{}'
+find /media/Music -type f ! -name "*.flac" -print0 | xargs -P0 -I '{}' -0 \
+    bash -c 'ln "$1" "$(echo "$1" | sed s/^\\\/media\\\/Music/\\\/media\\\/MusicLossy/)"' - '{}' \;
+
