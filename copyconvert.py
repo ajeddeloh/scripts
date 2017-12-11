fccc5f551725e29faede1f9b230ec506db92bff1
Andrew Jeddeloh <andrewplayssax@gmail.com>
2017-12-10 19:05:00 -0500

    copyconvert.py: dont covert album art
    it would be encoded as an video frame which android cannot handle.

--- copyconvert.py
+++ copyconvert.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import subprocess
+import string
 from concurrent.futures import ProcessPoolExecutor
 
 if len(sys.argv) != 3:
@@ -9,6 +10,9 @@ if len(sys.argv) != 3:
 
 SRC = sys.argv[1]
 DST = sys.argv[2]
+
+def translate_name(fname):
+    return "".join(ch if (ch.isalnum() or ch == "/" or ch == ".") else "_" for ch in fname)
 
 def get_all_files(src):
     for root, dirs, files in os.walk(SRC):
@@ -22,12 +26,14 @@ def make_dirs_for_file(path):
 
 def process_file(fname):
     #pass a tuple so it can be map'd with the Executor
-    print (fname)
+    #print (fname)
     relpath = os.path.relpath(fname, SRC)
     if fname.lower().endswith(".flac"):
         new_fname = os.path.join(DST, os.path.splitext(relpath)[0] + ".ogg")
+        new_fname = translate_name(new_fname)
         make_dirs_for_file(new_fname)
-        subprocess.call(["ffmpeg", "-hide_banner", "-loglevel", "quiet", "-y", "-i", fname, "-map", "0:a:0", "-acodec", "libopus", "-b:a", "96000", new_fname], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+        print(new_fname)
+        subprocess.call(["ffmpeg", "-hide_banner", "-loglevel", "quiet", "-n", "-i", fname, "-map", "0:a:0", "-acodec", "libopus", "-b:a", "96000", new_fname], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
     else:
         new_fname = os.path.join(DST, relpath)
         if os.path.exists(new_fname):
